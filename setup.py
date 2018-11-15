a583223c81834af742c6dca05b00a1b00be28a9f
Martin Czygan <martin.czygan@gmail.com>
2018-11-15 10:36:49 +0100

    add command line entry point

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(name='hellopkg',
       version='0.1.0',
       packages=['hellopkg'],
-      install_requires=['requests'])
+      install_requires=['requests'],
+      entry_points={
+          'console_scripts': ['hellopkg-cli=hellopkg.hello:hello'],
+      })
 
